9abaae2d5b328579472c9d92d3ba54ade1ef508c
zxj_best <zxj_best@163.com>
2014-10-06 21:07:02 +0800

    first commit

---
+++ src/main/java/com/zte/ums/agile/homework/tdd3/guess_number/GuessNumber.java
@@ -0,0 +1,10 @@
+package com.zte.ums.agile.homework.tdd3.guess_number;
+
+public class GuessNumber {
+
+    public String guess(String input) {
+        // TODO Auto-generated method stub
+        return "0A0B";
+    }
+
+}
--- src/test/java/com/zte/ums/agile/homework/tdd3/guess_number/GuessNumberTest.java
+++ src/test/java/com/zte/ums/agile/homework/tdd3/guess_number/GuessNumberTest.java
@@ -1,14 +1,26 @@
 package com.zte.ums.agile.homework.tdd3.guess_number;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
 
 public class GuessNumberTest {
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void test_should_return_0A0B_when_0A0B() {
+        GuessNumber guessNumber =  new GuessNumber();
+        String result = guessNumber.guess("1234");
+
+        assertThat(result, is("0A0B"));
+    }
+
+    @Test
+    public void test_should_return_0A1B_when_0A1B() {
+        GuessNumber guessNumber =  new GuessNumber();
+        String result = guessNumber.guess("9234");
+
+        assertThat(result, is("0A1B"));
     }
 
 }
