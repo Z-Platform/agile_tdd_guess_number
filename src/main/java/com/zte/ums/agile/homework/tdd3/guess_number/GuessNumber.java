ae7a5fdc6d8442f1760712cffdfb6fdf52339c74
zxj_best <zxj_best@163.com>
2014-10-06 22:22:50 +0800

    refactor 0A1B

--- src/main/java/com/zte/ums/agile/homework/tdd3/guess_number/GuessNumber.java
+++ src/main/java/com/zte/ums/agile/homework/tdd3/guess_number/GuessNumber.java
@@ -10,11 +10,12 @@ import com.google.common.collect.Sets;
 public class GuessNumber {
 
     public String guess(String input) {
-        List<String> bingoNumberList = Lists.newArrayList();
-        bingoNumberList.add("6");
-        bingoNumberList.add("7");
-        bingoNumberList.add("8");
-        bingoNumberList.add("9");
+        List<String> bingoNumberList = start();
+        int BCount = judgeBCount(input, bingoNumberList);
+        return "0A" + BCount + "B";
+    }
+
+    private int judgeBCount(String input, List<String> bingoNumberList) {
         Set<String> inputSet = Sets.newHashSet();
         for (int i = 0; i < input.length(); i++) {
             inputSet.add(String.valueOf(input.charAt(i)));
@@ -25,6 +26,16 @@ public class GuessNumber {
                 iterator.remove();
             }
         }
-        return "0A" + (4 - inputSet.size()) + "B";
+        int BCount = 4 - inputSet.size();
+        return BCount;
+    }
+
+    private List<String> start() {
+        List<String> bingoNumberList = Lists.newArrayList();
+        bingoNumberList.add("6");
+        bingoNumberList.add("7");
+        bingoNumberList.add("8");
+        bingoNumberList.add("9");
+        return bingoNumberList;
     }
 }
