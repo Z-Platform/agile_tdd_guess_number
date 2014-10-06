4662fd4aed093ea0706cdeaa121f0665278882af
zhouxiaojun <zxj_best@163.com>
2014-10-06 20:35:29 +0800

    first commit

---
+++ src/main/java/com/zte/ums/agile/homework/tdd3/guess_number/App.java
@@ -0,0 +1,13 @@
+package com.zte.ums.agile.homework.tdd3.guess_number;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/zte/ums/agile/homework/tdd3/guess_number/GuessNumberTest.java
@@ -0,0 +1,14 @@
+package com.zte.ums.agile.homework.tdd3.guess_number;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GuessNumberTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
